6dea68168fd060486f10e7a69d631500e741191d
Tyler Simeone <tyler.simeone@gmail.com>
2020-02-05 09:34:29 -0600

    minor update to main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,6 @@
 import DOM from "./domManager.js"
 
-// This code renders each obj in db arr to the DOM in DOM components
 // DOM.renderAllToDom();
-DOM.renderPurchasingAgents();
+// DOM.renderPurchasingAgents();
 // DOM.renderNyToDom();
 // DOM.renderManufacturing();
